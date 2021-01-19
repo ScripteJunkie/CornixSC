df792d0d559cc14982fed05d4e25d6ef5446f373
Ashton <30474771+ScripteJunkie@users.noreply.github.com>
2021-01-19 12:07:16 -0500

    Organization

--- getcoords.py
+++ getcoords.py
@@ -1,9 +1,20 @@
 import time
 import pyperclip
+from flask import Flask, render_template
+app = Flask(__name__)
 
 while(True):
     data = pyperclip.waitForNewPaste()
     data = data.split(' ')
     data.pop(0)
     data = float(data[0][2:]), float(data[1][2:]), float(data[2][2:])
-    print(time.time(), data[0], data[1], data[2])
+    data = time.time(), data[0], data[1], data[2]
+    print(data)
+
+
+@app.route('/')
+def main():
+    return render_template('index.html', data = data)
+
+if __name__ == '__main__':
+   app.run()
