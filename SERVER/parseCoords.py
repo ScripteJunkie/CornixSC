756fcadc06aa6cedf4c488a7fd0d894fd0921aa5
Ashton <30474771+ScripteJunkie@users.noreply.github.com>
2021-03-20 00:30:55 -0400

    Equations added

--- SERVER/parseCoords.py
+++ SERVER/parseCoords.py
@@ -4,9 +4,13 @@ import time
 x = 0
 y = 0
 z = 0
+
 def checker(x, y, z):
-     dist = ((x - avx)**2 + (y - avy)**2 + (z -avz)**2)**0.5
+     dist = ((x - avx)**2 + (y - avy)**2 + (z - avz)**2)**0.5
+
+     #dist = ((xc - x)**2 + (yc - y)**2 + (zc - z)**2)**0.5
      return round(dist, 6)
+
 with open(os.path.join(sys.path[0], "DaymarEquator.txt"), "r") as f:
     content = f.read().splitlines()
     flen = len(content)
@@ -16,8 +20,9 @@ with open(os.path.join(sys.path[0], "DaymarEquator.txt"), "r") as f:
             data = data.split(' ')
             data.pop(0)
             data = float(data[0][2:]), float(data[1][2:]), float(data[2][2:]), int(data[3][5:])
+            # time, x, y, z
             data = data[3], data[0], data[1], data[2]
-            print(data)
+            #print(data)
             x = x + data[1]
             y = y + data[2]
             z = z + data[3]
@@ -25,6 +30,6 @@ with open(os.path.join(sys.path[0], "DaymarEquator.txt"), "r") as f:
     avy = round(y / flen, 6)
     avz = round(z / flen, 6)
     print("Center: ", avx, avy, avz)
-    print(checker(-18930369755.629940, -2610400166.433840, -0.056059))
-    print(checker(-18930259733.400406, -2610252624.504298, -0.963677))
-    print(checker(-18930245132.941551, -2610179399.053881, -0.023946))
+    #print(checker(-18930369755.629940, -2610400166.433840, -0.056059))
+    #print(checker(-18930259733.400406, -2610252624.504298, -0.963677))
+    #print(checker(-18930245132.941551, -2610179399.053881, -0.023946))
