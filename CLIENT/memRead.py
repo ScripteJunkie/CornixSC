891e5731cc5ed1a34d81fccbcf61ca847132c560
Ashton <30474771+ScripteJunkie@users.noreply.github.com>
2021-07-19 00:01:41 -0400

    Major Coordinate Feature Overhaul
    Fixed the equations to determine planet center to have a 0.0003% error without even using Z component.
    Daymar's center is within 7.2 meter of (-18930539539.534645, -2610158765.4989395, 0)!

--- CLIENT/memRead.py
+++
@@ -1,7 +0,0 @@
-import pymem
-import os
-
-pm = pymem.Pymem('Spotify.exe')
-modules = list(pm.list_modules())
-for module in modules:
-    print(module.name)
--- SERVER/parseCoords.py
+++ SERVER/parseCoords.py
@@ -1,48 +1,49 @@
 import os
 import sys
-import time
-x = 0
-y = 0
-z = 0
+coords = []
 
+# checks distance(radius) between given point and solved center
 def checker(x, y, z):
-     dist = ((x - avx)**2 + (y - avy)**2 + (z - avz)**2)**0.5
-
-     #dist = ((xc - x)**2 + (yc - y)**2 + (zc - z)**2)**0.5
+     dist = ((x - avx)**2 + (y - avy)**2 + (z - 0)**2)**0.5
      return round(dist, 6)
 
+# solves for midpoint, slope, normal, and both y-intercepts between two points
+def normalSolver(index1, index2):
+     slope = (coords[index2][1]-coords[index1][1])/(coords[index2][0]-coords[index1][0])
+     normal = -1/slope
+     midp = (coords[index1][0]+coords[index2][0])/2, (coords[index1][1]+coords[index2][1])/2
+     yint = midp[1] - (slope * midp[0])
+     Nyint = midp[1] - (normal * midp[0])
+     # print("Slope: ", slope)
+     # print("Normal: ", normal)
+     # print("Midpoint: ", midp)
+     # print("Y-Int: ", yint)
+     # print("Normal Y-Int: ", Nyint)
+     return[slope, normal, midp, yint, Nyint]
+
+# returns coordinate of intersection of the normals of two set of points p1, p2 and p3, p4
+def intersection(p1, p2, p3, p4):
+     line1 = normalSolver(p1, p2)
+     line2 = normalSolver(p3, p4)
+     xi = (line1[4] - line2[4]) / (line2[1] - line1[1])
+     yi = line1[1] * xi + line1[4]
+     return[xi, yi]
+
+# writes DaymarEquator.txt into an array
 with open(os.path.join(sys.path[0], "DaymarEquator.txt"), "r") as f:
     content = f.read().splitlines()
     flen = len(content)
-#
-#    for l in content: 
-#            data = l
-#            data = data.split(' ')
-#            data.pop(0)
-#            data = float(data[0][2:]), float(data[1][2:]), float(data[2][2:]), int(data[3][5:])
-            # time, x, y, z
-#            data = data[3], data[0], data[1], data[2]
-            #print(data)
-#            x = x + data[1]
-#            y = y + data[2]
-#            z = z + data[3]
-    for i in range(5):
+    for i in range(flen):
             data = content[i]
             data = data.split(' ')
             data.pop(0)
             data = float(data[0][2:]), float(data[1][2:]), float(data[2][2:]), int(data[3][5:])
             # time, x, y, z
             data = data[3], data[0], data[1], data[2]
-            #print(data)
-            x = x + data[1]
-            y = y + data[2]
-            z = z + data[3] 
-    avx = x / flen
-    avy = y / flen
-    avz = z / flen
+            coords.append((data[1], data[2], data[3]))
 
-    print("Center: ", avx, avy, avz)
-    print(checker(-18930369755.629940, -2610400166.433840, -0.056059))
-    print(checker(-18930259733.400406, -2610252624.504298, -0.963677))
-    print(checker(-18930245132.941551, -2610179399.053881, -0.023946))
-    print(checker(-18930369755.629940, -2610400166.433840, -0.056059)-checker(-18930259733.400406, -2610252624.504298, -0.963677))
+# 
+    final = intersection(2, 3, 3, 4)
+    avx = final[0]
+    avy = final[1]
+    print("Center: ", avx, avy, "~0")
